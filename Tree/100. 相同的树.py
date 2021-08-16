738cb648ad3365f6e400c913e146de507afa6c3a
Barry <84859347+BarryLoveBerry@users.noreply.github.com>
2021-08-16 22:13:16 +0800

    Create 100. 相同的树.py

---
+++ Tree/100. 相同的树.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#DFS 相同的二叉树，当且仅当俩个二叉树的结构完全相同，并且所有节点的值相同
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not q and not p:
+            return True
+        elif not q or not p:
+            return False
+        elif p.val != q.val:
+            return False
+        else:
+            return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+#BFS
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        
+        queue1 = collections.deque([p])
+        queue2 = collections.deque([q])
+
+        while queue1 and queue2:
+            node1 = queue1.popleft()
+            node2 = queue2.popleft()
+            if node1.val != node2.val:
+                return False
+            left1, right1 = node1.left, node1.right
+            left2, right2 = node2.left, node2.right
+            if (not left1) ^ (not left2):
+                return False
+            if (not right1) ^ (not right2):
+                return False
+            if left1:
+                queue1.append(left1)
+            if right1:
+                queue1.append(right1)
+            if left2:
+                queue2.append(left2)
+            if right2:
+                queue2.append(right2)
+
+        return not queue1 and not queue2
