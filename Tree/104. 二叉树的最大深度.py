c6607340d3e0cbb7615a2c4abc2fb7c6afeee34d
Barry <84859347+BarryLoveBerry@users.noreply.github.com>
2021-08-16 20:29:35 +0800

    Create 104. 二叉树的最大深度.py

---
+++ Tree/104. 二叉树的最大深度.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Recursion DFS
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        else:
+            left_height = self.maxDepth(root.left)
+            right_height = self.maxDepth(root.right)
+            return max(left_height,right_height)+1
+          
+#BFS
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        queue = collections.deque([root])
+        depth = 0
+        while queue:
+            n = len(queue)
+            for i in range(n):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            depth += 1
+        return depth
